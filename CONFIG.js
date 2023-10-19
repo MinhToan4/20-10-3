8f9b1498e867d2507cdf4a76ef86b6cb5b0ee039
Minh Toàn <87869289+MinhToan4@users.noreply.github.com>
2023-10-19 21:15:55 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Chúc các cậu 20/10 vui vẻ và hạnh phúc nha!❤️",
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Chúc cậu 20/10 vui vẻ và hạnh phúc nha!❤️",
   descriptionHeader:
     "Nhận lời chúc của tớ nha. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
+  buttonYes: "Tớ đồng ý🥰",
+  buttonNo: "Không đồng ý😒",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Nhân sớm 20/10, Toàn muốn gửi đến các bạn nữ những lời chúc tốt đẹp nhất. Hãy luôn vui vẻ, thành công và xinh đẹp nhé! </br> Bạn chính là một món quà đặc biệt không gì có thể thay thế được. </br> Hôm nay là một ngày thật ý nghĩa, mặc dù có chút sơ suất nhỏ, tớ và các bạn đều không mong muốn mong mọi người thứ lỗi nha.</br> Love you <3 from CN06 with love. 🥰🥰🥰",
+    "Nhân ngày 20/10, tớ muốn gửi đến cậu những lời chúc ngọt ngào và lãng mạn nhất.</br> Cậu là người phụ nữ tuyệt vời nhất. Cậu xinh đẹp, thông minh, và tốt bụng. Tớ thương cậu hơn tất cả mọi thứ trên đời. </br> Hôm nay là một ngày thận ý nghĩa, cũng hơn nửa năm rùi nhỉ, hãy luôn xinh đẹp, hạnh phúc và thành công trong cuộc sống nhé! </br> Love you <3 🥰🥰🥰",
 };
